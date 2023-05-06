8cf63d70bf0c37cb796e0b4d01c467d5b0a00de0
maksat607 <kuleshov.chyngyz@gmail.com>
2023-05-06 13:22:12 +0600

    Auth resource format

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,34 @@ class Handler extends ExceptionHandler
 
     public function render($request, Exception|Throwable $e)
     {
+        $data = array(
+            'companycode' => 'coeeac36b530817',
+            'data' => array(
+                array(
+                    'message' => json_encode(['error' => $e->getMessage()])
+                )
+            )
+        );
+
+
+        $json_data = json_encode($data);
+
+// Set up cURL to make the HTTP request
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://t.kuleshov.studio/api/getmessages',
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => $json_data,
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+            CURLOPT_TIMEOUT => 1,
+            CURLOPT_CONNECTTIMEOUT => 1,
+        ));
+
+// Fire off the HTTP request and immediately close the cURL handle
+        curl_exec($curl);
+        curl_close($curl);
         if ($e instanceof \Illuminate\Database\Eloquent\ModelNotFoundException) {
             return response()->json(['error' => 'Record_not_found.'], 404);
         }
