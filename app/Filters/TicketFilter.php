77172b866350818ebe1f389070c22266ce6601f0
maksat607 <kuleshov.chyngyz@gmail.com>
2023-05-06 15:54:38 +0600

    add date validate

--- app/Filters/TicketFilter.php
+++ app/Filters/TicketFilter.php
@@ -117,88 +117,89 @@ class TicketFilter
 
     public function apply(Builder $builder)
     {
+
 //        $builder->skip($this->offset)->take($this->limit);
-//
-//        if ($this->fromAirport) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->where('from_airport_id', $this->fromAirport);
-//            });
-//        }
-//        if ($this->toAirport) {
-//            $builder->where('ticketAirplaneTicket', function ($query) {
-//                $query->where('to_airport_id', $this->toAirport);
-//            });
-//        }
-//        if ($this->fromStartDateAt) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->where('start_date_at', '>=', $this->fromStartDateAt);
-//            });
-//        }
-//        if ($this->toStartDateAt) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->where('end_date_at', '>=', $this->toStartDateAt);
-//            });
-//        }
-//        if ($this->isOnlyWithReturnWay) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->whereNotNull('is_one_way');
-//            });
-//        }
-//        if ($this->isOnlyWithoutReturnWay) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->whereNull('is_one_way');
-//            });
-//        }
-//
-//        if ($this->classType) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->where('class_type', $this->classType);
-//            });
-//        }
-//
-//
-//        if ($this->adultsCount) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->where('adults_count', $this->adultsCount);
-//            });
-//        }
-//
-//        if ($this->childrenCount) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->where('children_count', $this->childrenCount);
-//            });
-//        }
-//        if ($this->infantsCount) {
-//            $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                $query->where('infants_count', $this->infantsCount);
-//            });
-//        }
-//
-//        switch ($this->sortType) {
-//            case TicketSortType::PRICE_ASC:
-//                $builder->orderBy('price', 'asc');
-//                break;
-//            case TicketSortType::PRICE_DESC:
-//                $builder->orderBy('price', 'desc');
-//                break;
-//            case TicketSortType::DATE_ASC:
-//                $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                    $query->orderBy('start_date_at', 'asc');
-//                });
-//                break;
-//            case TicketSortType::DATE_DESC:
-//                $builder->whereHas('ticketAirplaneTicket', function ($query) {
-//                    $query->orderBy('start_date_at', 'desc');
-//                });
-//                break;
-//            default:
-//                $builder->orderBy('price', 'asc');
-//                break;
-//        }
-//
-//        if ($this->watcher) {
-//            $builder->where('watcher_id', $this->watcher->id);
-//        }
+
+        if ($this->fromAirport) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->where('from_airport_id', $this->fromAirport);
+            });
+        }
+        if ($this->toAirport) {
+            $builder->where('ticketAirplaneTicket', function ($query) {
+                $query->where('to_airport_id', $this->toAirport);
+            });
+        }
+        if ($this->fromStartDateAt) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->where('start_date_at', '>=', $this->fromStartDateAt);
+            });
+        }
+        if ($this->toStartDateAt) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->where('end_date_at', '>=', $this->toStartDateAt);
+            });
+        }
+        if ($this->isOnlyWithReturnWay) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->whereNotNull('is_one_way');
+            });
+        }
+        if ($this->isOnlyWithoutReturnWay) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->whereNull('is_one_way');
+            });
+        }
+
+        if ($this->classType) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->where('class_type', $this->classType);
+            });
+        }
+
+
+        if ($this->adultsCount) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->where('adults_count', $this->adultsCount);
+            });
+        }
+
+        if ($this->childrenCount) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->where('children_count', $this->childrenCount);
+            });
+        }
+        if ($this->infantsCount) {
+            $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                $query->where('infants_count', $this->infantsCount);
+            });
+        }
+
+        switch ($this->sortType) {
+            case TicketSortType::PRICE_ASC:
+                $builder->orderBy('price', 'asc');
+                break;
+            case TicketSortType::PRICE_DESC:
+                $builder->orderBy('price', 'desc');
+                break;
+            case TicketSortType::DATE_ASC:
+                $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                    $query->orderBy('start_date_at', 'asc');
+                });
+                break;
+            case TicketSortType::DATE_DESC:
+                $builder->whereHas('ticketAirplaneTicket', function ($query) {
+                    $query->orderBy('start_date_at', 'desc');
+                });
+                break;
+            default:
+                $builder->orderBy('price', 'asc');
+                break;
+        }
+
+        if ($this->watcher) {
+            $builder->where('watcher_id', $this->watcher->id);
+        }
 
         return $builder;
     }
