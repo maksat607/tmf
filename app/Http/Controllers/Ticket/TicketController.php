95848399cb7283bd08a4efe82d818e5e5b993e44
maksat607 <kuleshov.chyngyz@gmail.com>
2023-05-15 11:23:35 +0600

    Filters

--- app/Http/Controllers/Ticket/TicketController.php
+++ app/Http/Controllers/Ticket/TicketController.php
@@ -27,18 +27,18 @@ class TicketController extends Controller
 
     public function index(Request $request)
     {
-//        $cacheKey = $this->generateCacheKey('index_' . $request->getQueryString());
-//        $cacheDuration = 3600; // Cache duration in seconds (1 hour)
-//
-//        $response = Cache::remember($cacheKey, $cacheDuration, function () use ($request) {
+        $cacheKey = $this->generateCacheKey('index_' . $request->getQueryString());
+        $cacheDuration = 3600; // Cache duration in seconds (1 hour)
+
+        $response = Cache::remember($cacheKey, $cacheDuration, function () use ($request) {
             $items = $this->ticketService->index($request);
             $count = $items->count();
             $tickets = TicketResource::collection($items);
 
             return Response::json($tickets)->header('X-Total-Count', $count);
-//        });
-//
-//        return $response;
+        });
+
+        return $response;
     }
 
     public function store(StoreTicket $request)
